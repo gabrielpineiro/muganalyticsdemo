1818e630adaa844bc124f7b124f2910730f076e2
gabriel <gabrielpineiro@gmail.com>
2016-03-10 15:22:49 -0300

    primera versión con gráficos

---
+++ src/MUGAnalyticsDemo/MUGAnalyticsDemo.Web/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace MUGAnalyticsDemo.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Updates()
+        {
+            var documentDBEndpointUrl = ConfigurationManager.AppSettings["Analytics.DocumentDB.Endpoint"];
+            var documentDBAuthKey = ConfigurationManager.AppSettings["Analytics.DocumentDB.AccountKey"];
+
+            var client = new DocumentClient(new Uri(documentDBEndpointUrl), documentDBAuthKey);
+
+            var database = client.CreateDatabaseQuery().Where(db => db.Id == "analyticsdata").AsEnumerable().FirstOrDefault();
+
+            var documentCollection = client.CreateDocumentCollectionQuery("dbs/" + database.Id).Where(c => c.Id == "demoCollection").AsEnumerable().FirstOrDefault();
+
+            var results = client.CreateDocumentQuery(documentCollection.SelfLink, "SELECT c.UserName, c.Timestamp FROM c").ToList();
+
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(results);
+
+            return Content(string.Format("data: {0}\n\n", json), "text/event-stream");
+        }
+    }
+}
---
+++ src/MUGAnalyticsDemo/MUGAnalyticsDemo.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MUGAnalyticsDemo.Web.MvcApplication" Language="C#" %>
