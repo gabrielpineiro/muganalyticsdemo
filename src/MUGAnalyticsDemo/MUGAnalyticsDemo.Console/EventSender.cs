e23abb6ce2a7b19654430110e02290237fb08316
gabriel <gabrielpineiro@gmail.com>
2016-03-10 14:29:43 -0300

    primer push

---
+++ src/MUGAnalyticsDemo/MUGAnalyticsDemo.Console/EventSender.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MUGAnalyticsDemo.Console
+{
+    public static class EnumerableExtension
+    {
+        public static T PickRandom<T>(this IEnumerable<T> source)
+        {
+            return source.PickRandom(1).Single();
+        }
+
+        public static IEnumerable<T> PickRandom<T>(this IEnumerable<T> source, int count)
+        {
+            return source.Shuffle().Take(count);
+        }
+
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
+        {
+            return source.OrderBy(x => Guid.NewGuid());
+        }
+    }
+
+    public class EventSender
+    {
+        const int numberOfDevices = 1000;
+        string _eventHubName;
+        string _connectionString;
+        int _numberOfMessages;
+        private static Random random = new Random((int)DateTime.Now.Ticks);
+
+        private List<string> _users = new List<string>
+        {
+            "Leonardo", "Augusto", "Gabriel", "Alejandro", "Pedro", "Natalia", "Celeste", "Ruben", "Adrian", "Martin"
+        };
+
+        public EventSender(string connectionString, string eventHubName, int numberOfMessages)
+        {
+            _connectionString = connectionString;
+            _eventHubName = eventHubName;
+            _numberOfMessages = numberOfMessages;
+        }
+
+        public void SendEvents()
+        {
+            // Create EventHubClient
+            var client = EventHubClient.CreateFromConnectionString(_connectionString, _eventHubName);
+
+            // Send messages to Event Hub
+
+            Random random = new Random();
+            System.IO.MemoryStream ms;
+
+            for (int i = 0; i < _numberOfMessages; ++i)
+            {
+                var randomUser = _users.PickRandom();
+
+                var message = new
+                {
+                    UserName = randomUser,
+                    Timestamp = DateTime.UtcNow.ToString("s", CultureInfo.InvariantCulture) + "Z",
+                    id = Guid.NewGuid()
+                };
+
+                ms = new System.IO.MemoryStream();
+                System.IO.StreamWriter sw = new System.IO.StreamWriter(ms, Encoding.ASCII);
+
+                var jsonMessage = JsonConvert.SerializeObject(message, new JsonSerializerSettings
+                {
+                    NullValueHandling = NullValueHandling.Ignore
+                });
+
+                sw.Write(jsonMessage);
+                sw.Flush();
+                ms.Position = 0;
+
+                // Send the metric to Event Hub
+                client.Send(new EventData(ms));
+            }
+
+            client.Close();
+        }
+    }
+}
---
+++ src/MUGAnalyticsDemo/MUGAnalyticsDemo.Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MUGAnalyticsDemo.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int numberOfMessages = 100;
+            string connectionString = ConfigurationManager.AppSettings["Analytics.EventHub.ConnectionString"];
+            string eventHubName = ConfigurationManager.AppSettings["Analytics.EventHub.Name"];
+            int messageRetentionInDays = int.Parse(ConfigurationManager.AppSettings["Analytics.EventHub.MessageRetentionInDays"]);
+            int partitionCount = int.Parse(ConfigurationManager.AppSettings["Analytics.EventHub.PartitionCount"]);
+            string sharedAccessKeyName = ConfigurationManager.AppSettings["Analytics.EventHub.SharedAccessKeyName"];
+            string sharedAccessKey = ConfigurationManager.AppSettings["Analytics.EventHub.SharedAccessKey"];
+            string documentDBEndpointUrl = ConfigurationManager.AppSettings["Analytics.DocumentDB.Endpoint"];
+            string documentDBAuthKey = ConfigurationManager.AppSettings["Analytics.DocumentDB.AccountKey"];
+
+            var sender = new EventSender(connectionString, eventHubName, numberOfMessages);
+            sender.SendEvents();
+        }
+    }
+}
